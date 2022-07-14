f981313d15d1692103c4c7cee8a4b87a08f0c359
Pratik Kinage <pratikkinage@gmail.com>
2022-07-14 18:34:08 +0530

    release ready with build

--- build/blocks/number-counter/index.asset.php
+++ build/blocks/number-counter/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '37a883589bcfb0daa1c4');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'e3cd5aabe4c7cd4fd8d9');
