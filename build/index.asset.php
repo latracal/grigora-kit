d6a52517a3b0756515c24c9fb8ee1a3054eaece5
Pratik Kinage <pratikkinage@gmail.com>
2022-08-17 10:12:47 +0530

    scroll to top

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => '25f7cbb169d62b5fb95f');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => '2b65ea41768d45e9598e');
