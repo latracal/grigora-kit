e14994730f8fdcdc93cf727ce87774af13096596
Pratik Kinage <pratikkinage@gmail.com>
2022-08-23 18:55:10 +0530

    post category complete

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => '37d9a4a1cd0769bb7c06');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => 'f65daed428f7c1b32efa');
--- inc/blocks/generate-css/post-category.php
+++ inc/blocks/generate-css/post-category.php
@@ -231,15 +231,16 @@ if(!function_exists("ga_generate_css_post_category")){
                 $css = $css . sprintf("border-bottom-left-radius: %s;", $attributes['effectNBorderRadius']['bottomLeft']);
             }
         }
-        $css = $css . sprintf("transform: rotateX(%s) rotateY(%s) rotateZ(%s) skewX(%s) skewY(%s) translateX(%s) translateY(%s) scale(%s);",
-                (isset($attributes['effectNRotateX']) && $attributes['effectNRotateX']) ? $attributes['effectNRotateX'] : '0deg',
-                (isset($attributes['effectNRotateY']) && $attributes['effectNRotateY']) ? $attributes['effectNRotateY'] : '0deg',
-                (isset($attributes['effectNRotateZ']) && $attributes['effectNRotateZ']) ? $attributes['effectNRotateZ'] : '0deg',
-                (isset($attributes['effectNSkewX']) && $attributes['effectNSkewX']) ? $attributes['effectNSkewX'] : '0deg',
-                (isset($attributes['effectNSkewY']) && $attributes['effectNSkewY']) ? $attributes['effectNSkewY'] : '0deg',
-                (isset($attributes['effectNOffsetX'])) ? $attributes['effectNOffsetX'] : '0',
-                (isset($attributes['effectNOffsetY'])) ? $attributes['effectNOffsetY'] : '0',
-                (isset($attributes['effectNScale'])) ? $attributes['effectNScale'] : '1',
+
+        $css = $css . sprintf("transform: %s %s %s %s %s %s %s %s;",
+                (isset($attributes['effectNRotateX']) && $attributes['effectNRotateX']) ? "rotateX({$attributes['effectNRotateX']})" : '',
+                (isset($attributes['effectNRotateY']) && $attributes['effectNRotateY']) ? "rotateY({$attributes['effectNRotateY']})" : '',
+                (isset($attributes['effectNRotateZ']) && $attributes['effectNRotateZ']) ? "rotateZ({$attributes['effectNRotateZ']})" : '',
+                (isset($attributes['effectNSkewX']) && $attributes['effectNSkewX']) ? "skewX({$attributes['effectNSkewX']})" : '',
+                (isset($attributes['effectNSkewY']) && $attributes['effectNSkewY']) ? "skewY({$attributes['effectNSkewY']})" : '',
+                (isset($attributes['effectNOffsetX'])) ? "translateX({$attributes['effectNOffsetX']})" : '',
+                (isset($attributes['effectNOffsetY'])) ? "translateY({$attributes['effectNOffsetY']})" : '',
+                (isset($attributes['effectNScale'])) ? "scale({$attributes['effectNScale']})" : '',
         );
         $css = $css . sprintf("box-shadow: %s %s %s %s %s;",
         (isset($attributes['effectNShadowHO'])) ? $attributes['effectNShadowHO'] : '0px',
@@ -345,16 +346,17 @@ if(!function_exists("ga_generate_css_post_category")){
             (isset($attributes['effectHOffsetX']) && $attributes['effectHOffsetX'] ) ||
             (isset($attributes['effectHOffsetY']) && $attributes['effectHOffsetY'] ) ||
             (isset($attributes['effectHScale']) && $attributes['effectHScale'] )
-        ){            
-            $css = $css . sprintf("transform: rotateX(%s) rotateY(%s) rotateZ(%s) skewX(%s) skewY(%s) translateX(%s) translateY(%s) scale(%s);",
-                    (isset($attributes['effectHRotateX']) && $attributes['effectHRotateX']) ? $attributes['effectHRotateX'] : ((isset($attributes['effectNRotateX']) && $attributes['effectNRotateX']) ? $attributes['effectNRotateX'] : '0deg'),
-                    (isset($attributes['effectHRotateY']) && $attributes['effectHRotateY']) ? $attributes['effectHRotateY'] : ((isset($attributes['effectNRotateY']) && $attributes['effectNRotateY']) ? $attributes['effectNRotateY'] : '0deg'),
-                    (isset($attributes['effectHRotateZ']) && $attributes['effectHRotateZ']) ? $attributes['effectHRotateZ'] : ((isset($attributes['effectNRotateZ']) && $attributes['effectNRotateZ']) ? $attributes['effectNRotateZ'] : '0deg'),
-                    (isset($attributes['effectHSkewX']) && $attributes['effectHSkewX']) ? $attributes['effectHSkewX'] : ((isset($attributes['effectNSkewX']) && $attributes['effectNSkewX']) ? $attributes['effectNSkewX'] : '0deg'),
-                    (isset($attributes['effectHSkewY']) && $attributes['effectHSkewY']) ? $attributes['effectHSkewY'] : ((isset($attributes['effectNSkewY']) && $attributes['effectNSkewY']) ? $attributes['effectNSkewY'] : '0deg'),
-                    (isset($attributes['effectHOffsetX']) && $attributes['effectHOffsetX']) ? $attributes['effectHOffsetX'] : ((isset($attributes['effectNOffsetX']) && $attributes['effectNOffsetX']) ? $attributes['effectNOffsetX'] : '0'),
-                    (isset($attributes['effectHOffsetY']) && $attributes['effectHOffsetY']) ? $attributes['effectHOffsetY'] : ((isset($attributes['effectNOffsetY']) && $attributes['effectNOffsetY']) ? $attributes['effectNOffsetY'] : '0'),
-                    (isset($attributes['effectHScale'])) ? $attributes['effectHScale'] : ((isset($attributes['effectNScale']) && $attributes['effectNScale']) ? $attributes['effectNScale'] : '1'),
+        ){          
+              
+            $css = $css . sprintf("transform: %s %s %s %s %s %s %s %s;",
+                    (isset($attributes['effectHRotateX']) && $attributes['effectHRotateX']) ? "rotateX({$attributes['effectHRotateX']})" : ((isset($attributes['effectNRotateX']) && $attributes['effectNRotateX']) ? "rotateX({$attributes['effectNRotateX']})" : ''),
+                    (isset($attributes['effectHRotateY']) && $attributes['effectHRotateY']) ? "rotateY({$attributes['effectHRotateY']})" : ((isset($attributes['effectNRotateY']) && $attributes['effectNRotateY']) ? "rotateY({$attributes['effectNRotateY']})" : ''),
+                    (isset($attributes['effectHRotateZ']) && $attributes['effectHRotateZ']) ? "rotateZ({$attributes['effectHRotateZ']})" : ((isset($attributes['effectNRotateZ']) && $attributes['effectNRotateZ']) ? "rotateZ({$attributes['effectNRotateZ']})" : ''),
+                    (isset($attributes['effectHSkewX']) && $attributes['effectHSkewX']) ? "skewX({$attributes['effectHSkewX']})" : ((isset($attributes['effectNSkewX']) && $attributes['effectNSkewX']) ? "skewX({$attributes['effectNSkewX']})" : ''),
+                    (isset($attributes['effectHSkewY']) && $attributes['effectHSkewY']) ? "skewY({$attributes['effectHSkewY']})" : ((isset($attributes['effectNSkewY']) && $attributes['effectNSkewY']) ? "skewY({$attributes['effectNSkewY']})" : ''),
+                    (isset($attributes['effectHOffsetX']) && $attributes['effectHOffsetX']) ? "translateX({$attributes['effectHOffsetX']})" : ((isset($attributes['effectNOffsetX']) && $attributes['effectNOffsetX']) ? "translateX({$attributes['effectNOffsetX']})" : ''),
+                    (isset($attributes['effectHOffsetY']) && $attributes['effectHOffsetY']) ? "translateY({$attributes['effectHOffsetY']})" : ((isset($attributes['effectNOffsetY']) && $attributes['effectNOffsetY']) ? "translateY({$attributes['effectNOffsetY']})" : ''),
+                    (isset($attributes['effectHScale'])) ? "scale({$attributes['effectHScale']})" : ((isset($attributes['effectNScale']) && $attributes['effectNScale']) ? "scale({$attributes['effectNScale']})" : ''),
             );
         }
         $css = $css . "}";
