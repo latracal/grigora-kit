508128e384098fc5ece5f53db8fb73eec7edf512
Pratik Kinage <pratikkinage@gmail.com>
2022-08-24 12:41:03 +0530

    dummy block

---
+++ src/blocks/dummy-block/save.js
@@ -0,0 +1,17 @@
+import classnames from 'classnames';
+
+import { useBlockProps } from '@wordpress/block-editor';
+
+import parse from 'html-react-parser';
+
+import SVGIcons from '@constants/icons.json';
+
+export default function save( { attributes, className } ) {
+	const {
+		id,
+	} = attributes;
+
+	return (
+		null
+	);
+}
