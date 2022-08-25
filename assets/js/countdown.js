76c153cdb259b3fea8afc23f966faade6909a43e
DEEPAKINDRESH N <deepakindreshn@gmail.com>
2022-08-25 17:25:29 +0530

    Countdown.js began to work

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -15,95 +15,46 @@ var __assign =
 		return __assign.apply( this, arguments );
 	};
 
-    var CountDown = /** @class */ ( function () {
-        function CountDown( target, date, options ) {
-            var _this = this;
-            this.options = options
-            this.defaults = {
-                countdownDate: '',
-                countdownOnComplete: 'hide',
-                onCompleteURL: '',
-                completedState: false,
-                divider: false,
-                dividerCharacter: '',
-                format: 1,
-                orientation: 'block',
-                hideDays: false,
-                hideHours: false,
-                hideMinutes: false,
-                dayLabel: 'DAYS',
-                hourLabel: 'HRS',
-                minuteLabel: 'MINS',
-                secondLabel: 'SECS',
-                numSuffix: '',
-                numPrefix: '',
-            }
-            this.el =
-			typeof target === 'string'
-				? document.getElementById( target )
-				: target;
-        }
-
-        CountDown.prototype.start = function (callback) {
-            console.log("Prototype start activated")
-            if ( this.error ) {
-                return;
-            }
-            this.callback = callback;
-            var countDownDate = new Date(this.countdownDate).getTime();
-
-            // Update the count down every 1 second
-            var timer = setInterval(function() {
-
-            // Get today's date and time
-            var now = new Date().getTime();
-                
-            // Find the distance between now and the count down date
-            var distance = countDownDate - now;
-                
-            // Time calculations for days, hours, minutes and seconds
-            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-                
-            // Output the result in an element with id="demo"
-            console.log(days + "d " + hours + "h "
-            + minutes + "m " + seconds + "s ");
-
-            this.printValue(countDownDate)
-                
-            // If the count down is over, write some text 
-            if (distance < 0) {
-                clearInterval(timer);
-                console.log("EXPIRED");
-            }
-            }, 1000);
-        }
-
-        CountDown.prototype.printValue = function ( date ) {
-            var result = this.formattingFn( date );
-            if (
-                this.el.tagName === 'text' ||
-                this.el.tagName === 'tspan'
-            ) {
-                this.el.textContent = result;
-            } else {
-                this.el.innerHTML = result;
-            }
-        };
-            
-
-    return CountDown;
-    
-    } )();
-
-
 
 window.addEventListener( 'load', function () {
 	const elements = document.getElementsByClassName(
 		'wp-block-grigora-kit-countdown'
 	);
+
+    function startCountdown( date, id ) {
+        var countdownDate = new Date(date).getTime();
+
+        // Update the count down every 1 second
+        var timer = setInterval(function() {
+
+        // Get today's date and time
+        var now = new Date().getTime();
+            
+        // Find the distance between now and the count down date
+        var distance = countdownDate - now;
+            
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            
+        // Output the result in an element with id="demo"
+        // console.log(days + "d " + hours + "h "
+        // + minutes + "m " + seconds + "s ");
+
+
+        document.getElementById(id).innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+            
+        // If the count down is over, write some text 
+        if (distance < 0) {
+            clearInterval(timer);
+            console.log("EXPIRED");
+        }
+        }, 1000);
+        return 
+    }
 
 	function assignNewCountdownInstance(
 		id,
@@ -125,50 +76,12 @@ window.addEventListener( 'load', function () {
 		numPrefix,
 		numSuffix,
 	) {
-		function formatNumber( date ) {
-            var countdownDate = new Date(date).getTime();
+		
 
-            // Update the count down every 1 second
-            var timer = setInterval(function() {
+		
 
-            // Get today's date and time
-            var now = new Date().getTime();
-                
-            // Find the distance between now and the count down date
-            var distance = countdownDate - now;
-                
-            // Time calculations for days, hours, minutes and seconds
-            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-                
-            // Output the result in an element with id="demo"
-            console.log(days + "d " + hours + "h "
-            + minutes + "m " + seconds + "s ");
-                
-            // If the count down is over, write some text 
-            if (distance < 0) {
-                clearInterval(timer);
-                console.log("EXPIRED");
-            }
-            }, 1000);
-			return `${ numPrefix } ${days}d  ${hours}h ${months}m  ${seconds}s ${ numSuffix }`;
-		}
+        
 
-		let params = {
-			enableScrollSpy: true,
-			scrollSpyOnce: true,
-			prefix: numPrefix,
-			suffix: numSuffix,
-            countdownDate: countdownDate,
-		};
-
-        if (countdownDate){
-            params[ 'formattingFn' ] = formatNumber;
-        }
-
-		new CountDown( id, countdownDate, params );
 	}
 	for ( var i = 0; i < elements.length; i++ ) {
 		let id = elements[ i ].dataset.id;
@@ -190,6 +103,10 @@ window.addEventListener( 'load', function () {
         let countdownOnComplete = elements[ i ].dataset.oncomplete;
         let onCompleteURL = elements[ i ].dataset.oncompleteurl;
 
+        // console.log("Date is ",countdownDate);
+        startCountdown(countdownDate, id);
+        
+        // console.log("Accessing span element", document.getElementById(id))
 
 		assignNewCountdownInstance(
 			id,
